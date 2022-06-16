9c5b8c2e37f845eccfa8fc28ac90a3adedfa6026
Omggera <omggera@gmail.com>
2022-06-17 02:06:17 +0300

    осталось ШК коробок, оптата и submit

--- DostavkaNaMarket/Models/User.cs
+++ DostavkaNaMarket/Models/User.cs
@@ -21,12 +21,35 @@ namespace DostavkaNaMarket.Models
             Понедельник,
             Среда,
         }
-        public Date? DateSell { get; set; } = Date.Понедельник;
+        [Required(ErrorMessage = "Место и дату отправки нужно выбрать обязательно")]
+        public Date? DateSell { get; set; } = null;
 
         public DateTime date = DateTime.Now;
         public DateTime dateWednesday = new DateTime();
         public DateTime dateMonday = new DateTime();
 
+        [Required(ErrorMessage = "Поле ФИО обязательно для заполнения")]
+        public string ClientName { get; set; }
+
+        [Required(ErrorMessage = "Поле Номер телефона обязательно для заполнения")]
+        [MaxLength(10, ErrorMessage = "Номер телефона должен быть в формате 9011418686")]
+        [DataType(DataType.PhoneNumber)]
+        public string ClientPhone { get; set; }
+
+        public string ClientMail { get; set; }
+
+        public enum OfficeOrDelivery
+        {
+            Sam,
+            Vivoz,
+        }
+        public OfficeOrDelivery? OfDelSell { get; set; } = OfficeOrDelivery.Sam;
+        public Dictionary<string, object> InputAttributes { get; set; }
+        
+        public string ClientAdress { get; set; }
+        
+
+        // Не красивый, но рабочий метод определения даты отправки
         public void DateDelivery()
         {
             if ((date.ToString("dddd")) == "понедельник")
@@ -65,16 +88,23 @@ namespace DostavkaNaMarket.Models
                 dateWednesday = date.AddDays(3);
             }
         }
-        
 
-        
-
-        [Required(ErrorMessage = "Поле ФИО обязательно для заполнения")]
-        public string ClientName { get; set; }
-
-        [Required(ErrorMessage = "Поле Номер телефона обязательно для заполнения")]
-        [MaxLength(10, ErrorMessage = "Номер телефона должен быть в формате 9011418686")]
-        [DataType(DataType.PhoneNumber)]
-        public string ClientPhone { get; set; }
+        public void OnSelected()
+        {
+            if (OfDelSell is OfficeOrDelivery.Sam)
+            {
+                InputAttributes = new Dictionary<string, object>()
+                {
+                    { "style", "display:none;" }
+                };
+            }
+            else if (OfDelSell is OfficeOrDelivery.Vivoz)
+            {
+                InputAttributes = new Dictionary<string, object>()
+                {
+                    { "style", "display:inline;" }
+                };
+            }
+        }
     }
 }
